434969920ba94646d30e83c39912db4c5f161728
t4sn33m-s4h4t <mdsahat6397@gmail.com>
2025-02-20 16:26:28 +0600

    first commit

---
+++ Task-Management-Backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+ 
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+module.exports = app;
